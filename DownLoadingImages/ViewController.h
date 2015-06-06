2f7a3c6f124f9457fe825a0d78f6005bcc51e69d
何天雄 <823297222@qq.com>
2015-06-06 09:01:05 +0800

    Initial Commit

---
+++ DownLoadingImages/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DownLoadingImages
+//
+//  Created by zhhz on 15/6/6.
+//  Copyright (c) 2015年 zhhz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DownLoadingImages/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DownLoadingImages
+//
+//  Created by zhhz on 15/6/6.
+//  Copyright (c) 2015年 zhhz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
